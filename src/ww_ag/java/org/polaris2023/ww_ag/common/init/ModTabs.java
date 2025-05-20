6d2fcdb11821b52ba4e884cc0171989c56d0aa7f
baka4n <qq474899581@gmail.com>
2025-05-20 22:50:27 +0800

    简化

--- src/ww_ag/java/org/polaris2023/ww_ag/common/init/ModTabs.java
+++ src/ww_ag/java/org/polaris2023/ww_ag/common/init/ModTabs.java
@@ -1,9 +1,14 @@
 package org.polaris2023.ww_ag.common.init;
 
+import com.tterrag.registrate.util.entry.ItemProviderEntry;
 import dev.xkmc.l2core.init.reg.registrate.SimpleEntry;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.polaris2023.ww_ag.WWAgMod;
+
+import java.util.Locale;
+import java.util.function.Supplier;
 
 import static org.polaris2023.ww_ag.WWAgMod.REGISTRATE;
 
@@ -12,18 +17,29 @@ import static org.polaris2023.ww_ag.WWAgMod.REGISTRATE;
  * {@code @Date 2025/05/20 18:08:12}
  */
 public class ModTabs {
-    public static final SimpleEntry<CreativeModeTab> BUILDING_BLOCK = REGISTRATE.buildModCreativeTab("building_block", "Wild Wind: Building Block", b -> {
-        b.icon(ModBlocks.SALT_BLOCK::asStack);
-    });
-    public static final SimpleEntry<CreativeModeTab> NATURAL_BLOCKS = REGISTRATE.buildModCreativeTab("natural_blocks", "Wild Wind: Natural Blocks", b -> {
-        b.icon(ModBlocks.SALT_ORE::asStack);
-    });
-    public static final SimpleEntry<CreativeModeTab> INGREDIENTS = REGISTRATE.buildModCreativeTab("ingredients", "Wild Wind: Ingredients", b -> {
-        b.icon(ModItems.SALT::asStack);
-    });
-    public static final SimpleEntry<CreativeModeTab> FOOD_AND_DRINK = REGISTRATE.buildModCreativeTab("food_and_drink", "Wild Wind: Food And Drink", b -> {
-        b.icon(ModItems.BAKED_APPLE::asStack);
-    });
+    public static final SimpleEntry<CreativeModeTab> BUILDING_BLOCK = register(
+            "Building Block", ModBlocks.SALT_BLOCK::asStack
+    );
+    public static final SimpleEntry<CreativeModeTab> NATURAL_BLOCKS = register(
+            "Natural Blocks", ModBlocks.SALT_ORE::asStack
+    );
+    public static final SimpleEntry<CreativeModeTab> INGREDIENTS = register(
+            "Ingredients", ModItems.SALT::asStack
+    );
+    public static final SimpleEntry<CreativeModeTab> FOOD_AND_DRINK = register(
+            "Food And Drink", ModItems.SALT::asStack
+    );
+
+    public static SimpleEntry<CreativeModeTab> register(String name, String display, Supplier<ItemStack> supplier) {
+        return REGISTRATE.buildModCreativeTab(name, "Wild Wind: " + display, b -> {
+            b.icon(supplier);
+        });
+    }
+
+    public static SimpleEntry<CreativeModeTab> register(String display, Supplier<ItemStack> supplier) {
+        String name = display.toLowerCase(Locale.ROOT).replace(" ", "_").replace("-", "_").replace("$", "_");
+        return register(name, display, supplier);
+    }
 
 
 
